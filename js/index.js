b1189712648253b66f0fdf58d58f4eb613646afd
JunchunDai <daijunchun2675@126.com>
2018-04-15 15:30:16 +0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+      $("#fullpage").fullpage({
+          navigation: true,
+          css3: true,
+          anchors: ["page1", "page2", "page3", "page4", "page5", "page6", "page7"],
+
+        }
+      }
