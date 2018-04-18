4698bcf1bba85b350220d183ef6d200744ab2792
JunchunDai <daijunchun2675@126.com>
2018-04-18 15:13:33 +0800

    地图修复

--- js/tools.js
+++ js/tools.js
@@ -1,9 +1,14 @@
 $(document).ready(function () {
 
-           
+                        $("#marquee1").slide({
+                                effect: "scrollx",
+                                auto: true,
+                                seamless: true,
+                                beLock: true
+                        });
 
 
 
 
 
-        }
+                }
