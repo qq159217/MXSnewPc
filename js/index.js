d36756aa7cdbb8410e3c307253e51248cee8fbb7
JunchunDai <daijunchun2675@126.com>
2018-04-27 07:27:21 +0800

    ajax xiufu

--- js/index.js
+++ js/index.js
@@ -12,99 +12,55 @@ $(function () {
 $("#top_homepage").load("../site/common/common.html")
 
 // feedback 表單提交
- $("#feedbackBtn").click(function () {
+// $("#feedbackBtn").click(function () {
 
-	function p(s) {
-		return s < 10 ? '0' + s : s;
-	}
+// 	function p(s) {
+// 		return s < 10 ? '0' + s : s;
+// 	}
 
-	function GetNowTime() {
-		var myDate = new Date();
-		//获取当前年
-		var year = myDate.getFullYear();
-		//获取当前月
-		var month = myDate.getMonth() + 1;
-		//获取当前日
-		var date = myDate.getDate();
-		var h = myDate.getHours(); //获取当前小时数(0-23)
-		var m = myDate.getMinutes(); //获取当前分钟数(0-59)
-		var s = myDate.getSeconds();
-		return year + '-' + p(month) + "-" + p(date) + " " + p(h) + ':' + p(m) + ":" + p(s);
-	}
+// 	function GetNowTime() {
+// 		var myDate = new Date();
+// 		//获取当前年
+// 		var year = myDate.getFullYear();
+// 		//获取当前月
+// 		var month = myDate.getMonth() + 1;
+// 		//获取当前日
+// 		var date = myDate.getDate();
+// 		var h = myDate.getHours(); //获取当前小时数(0-23)
+// 		var m = myDate.getMinutes(); //获取当前分钟数(0-59)
+// 		var s = myDate.getSeconds();
+// 		return year + '-' + p(month) + "-" + p(date) + " " + p(h) + ':' + p(m) + ":" + p(s);
+// 	}
 
-	let requireData = $.md5("mixiusi" + GetNowTime());
+// 	let requireData = $.md5("mixiusi" + GetNowTime());
 
-	$("#hide").attr("value", requireData);
+// 	$("#hide").attr("value", requireData);
 
-	// ceshi 
-	let newData = $('#feedbackForm').serialize();
-	console.log(newData)
-
-
-
-	$.ajax({
-
-		type: "POST",
-		url: 'http://mixiusi.com.cn:8081/feedback/addFeedback',
-		data: newData,
-		success: function (result) {
-			// console.log(result); 
-			if (result.resultCode == 0000) {
-				window.location.href = "./site/submit_success/submitSucseccfully.html";
-			};
-		},
+// 	// ceshi 
+// 	let newData = $('#feedbackForm').serialize();
+// 	console.log(newData)
 
 
-	});
+
+// 	$.ajax({
+
+// 		type: "POST",
+// 		url: 'http://mixiusi.com.cn:8081/feedback/addFeedback',
+// 		data: newData,
+// 		success: function (result) {
+// 			// console.log(result); 
+// 			if (result.resultCode == 0000) {
+// 				window.location.href = "./site/submit_success/submitSucseccfully.html";
+// 			};
+// 		},
+
+
+// 	});
 
 
 
 	// 加盟合作form 提交
-	 $("#joinSubmit").click(function () {
-
-	 			function p(s) {
-	 				return s < 10 ? '0' + s : s;
-	 			}
-
-	 			function GetNowTime() {
-	 				var myDate = new Date();
-	 				//获取当前年
-	 				var year = myDate.getFullYear();
-	 				//获取当前月
-	 				var month = myDate.getMonth() + 1;
-	 				//获取当前日
-	 				var date = myDate.getDate();
-	 				var h = myDate.getHours(); //获取当前小时数(0-23)
-	 				var m = myDate.getMinutes(); //获取当前分钟数(0-59)
-	 				var s = myDate.getSeconds();
-	 				return year + '-' + p(month) + "-" + p(date) + " " + p(h) + ':' + p(m) + ":" + p(s);
-	 			}
-
-	 			let requireData = $.md5("mixiusi" + GetNowTime());
-
-	 			$("#hide").attr("value", requireData);
-
-	 			// ceshi 
-				 let newData = $('#joinForm').serialize();
-				 
-	 			console.log(newData)
-
-
-
-	 			$.ajax({
-
-	 				type: "POST",
-	 				url: 'http://47.98.164.44:8081/joinUs/addJoiner',
-	 				data: newData,
-	 				success: function (result) {
-	 					// console.log(result); 
-	 					if (result.resultCode == 0000) {
-	 						window.location.href = "./site/submit_success/submitSucseccfully.html";
-	 					};
-	 				},
-
-
-	 			});
+	
 
 
 
@@ -113,22 +69,4 @@ $("#top_homepage").load("../site/common/common.html")
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-});
+})
--- js/siteCommon.js
+++ js/siteCommon.js
@@ -1,5 +1,14 @@
 $(function () {
-  $('#top').load('../common/common.html');
+/*   $('nav>ul .item').mouseenter(function () {
+    $(this).find('.sub-menu').show()
+    $(this).siblings().find('.sub-menu').hide()
+  }) */
+  // 公共頁面加載
+   $('#top').load('../common/common.jsp')
+
+
+
+
 
   $('#marquee2').slide({
     scrollLen: 1,
@@ -9,45 +18,10 @@ $(function () {
     interval: 1000,
     lazyload: true
 
-  });
+  })
 
 
-
-
-  // 加盟注册表单提交
-  $("#joinSubmit").click(function () {
-    function p(s) {
-      return s < 10 ? '0' + s : s;
-    }
-
-    function GetNowTime() {
-      var myDate = new Date();
-      //获取当前年
-      var year = myDate.getFullYear();
-      //获取当前月
-      var month = myDate.getMonth() + 1;
-      //获取当前日
-      var date = myDate.getDate();
-      var h = myDate.getHours(); //获取当前小时数(0-23)
-      var m = myDate.getMinutes(); //获取当前分钟数(0-59)
-      var s = myDate.getSeconds();
-      return year + '-' + p(month) + "-" + p(date) + " " + p(h) + ':' + p(m) + ":" + p(s);
-    }
-    let requireData = $.md5("mixiusi" + GetNowTime());
-    $("#hide").attr("value", requireData);
-    // ceshi 
-    let newData = $('joinForm').serialize();
-    console.log(newData)
-    $.ajax({
-      type: "POST",
-      url: "http://mixiusi.com.cn:8081/feedback/addFeedback",
-      data: newData,
-      success: function (result) {
-        
-        if (result.resultCode == 0000) {
-          window.location.href = "../site/submit_success/submitSucseccfully.html";
-        };
-      },
-    });
-  });
-});
+  $('.messageR').click(function () {
+    console.log('test')
+  })
+})
