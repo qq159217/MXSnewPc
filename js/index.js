09e1df18ebcf50d75f8e5adcfc8e9b5511816a78
JunchunDai <daijunchun2675@126.com>
2018-04-28 11:34:36 +0800

    ui修改

--- js/index.js
+++ js/index.js
@@ -1,91 +1,74 @@
 $(function () {
+	$("#top").load("./site/common/common.html")
 
+  // 導航欄
+  $('nav>ul .item').mouseenter(function () {
+    $(this).find('.sub-menu').show()
+    $(this).siblings().find('.sub-menu').hide()
+  })
 
-	// 導航欄
-	$('nav>ul .item').mouseenter(function () {
-		$(this).find('.sub-menu').show()
-		$(this).siblings().find('.sub-menu').hide()
-	})
+  $('#top_homepage').load('../site/common/common.html')
 
+  $('#dowebok').fullpage({
+    menu: '#topNav',
+    css3: true,
+    scrollingSpeed: 700,
+    autoScrolling: true,
+    navigation: true
 
+  })
 
-$("#top_homepage").load("../site/common/common.html")
+  // 表單提交
+  $('#feedbackBtn').click(function () {
+    function p (s) {
+      return s < 10 ? '0' + s : s
+    }
 
-$("#dowebok").fullpage({
-	menu: '#topNav',
-	css3: true,
-		scrollingSpeed: 700,
-		autoScrolling: true,
-		navigation: true,
+    function GetNowTime () {
+      var myDate = new Date()
+      var year = myDate.getFullYear()
+      var month = myDate.getMonth() + 1
+      var date = myDate.getDate()
+      var h = myDate.getHours()
+      var m = myDate.getMinutes().toString().substring(0, 2)
+      var s = 0
+      return year + '-' + p(month) + '-' + p(date) + ' ' + p(h) + ':' + p(m)
+    }
 
-});
+    var phoneNumber = $('#ph').val()
+    console.log(phoneNumber)
+    var requireData = $.md5('mixiusi' + GetNowTime() + phoneNumber)
 
-
-
-
-
-
-
-
-
-
-
-// feedback 表單提交
-$("#feedbackBtn").click(function () {
-
-	function p(s) {
-		return s < 10 ? '0' + s : s;
-	}
-
-	function GetNowTime() {
-		var myDate = new Date();
-		//获取当前年
-		var year = myDate.getFullYear();
-		//获取当前月
-		var month = myDate.getMonth() + 1;
-		//获取当前日
-		var date = myDate.getDate();
-		var h = myDate.getHours(); //获取当前小时数(0-23)
-		var m = myDate.getMinutes(); //获取当前分钟数(0-59)
-		var s = myDate.getSeconds();
-		return year + '-' + p(month) + "-" + p(date) + " " + p(h) + ':' + p(m) + ":" + p(s);
-	}
-
-	let requireData = $.md5("mixiusi" + GetNowTime());
-
-	$("#hide").attr("value", requireData);
-
-	// ceshi 
-	let newData = $('#feedbackForm').serialize();
-	console.log(newData)
-
-
-
-// 	$.ajax({
-
-// 		type: "POST",
-// 		url: 'http://mixiusi.com.cn:8081/feedback/addFeedback',
-// 		data: newData,
-// 		success: function (result) {
-// 			// console.log(result); 
-// 			if (result.resultCode == 0000) {
-// 				window.location.href = "./site/submit_success/submitSucseccfully.html";
-// 			};
-// 		},
-
-
-// 	});
-
-
-
-	// 加盟合作form 提交
-	
-
-
-
-
-
-
-
-
+    $('#hide').attr('value', requireData)
+    var newData = $('#feedbackForm').serialize()
+    $.ajax({
+      type: 'POST',
+      url: 'http://47.98.164.44:8081/feedback/addFeedback',
+      data: newData,
+      success: function (result) {
+        if (result.code = '0000') {
+          console.log('加盟成功')
+          window.location.href = '../submit_success/submitSucseccfully.html'
+          $('#reset').reset()
+        } else if (result.code = '0001') {
+          console.log('代碼執行異常')
+        } else if (result.code = '0002') {
+          console.log('电话号码格式錯誤')
+          $('#ph').HTML('电话号码格式错误')
+        } else if (result.code = '0003') {
+          console.log('用戶名為空')
+          $('#nickName').HTML('用戶名為空')
+        } else if (result.code = '0004') {
+          $('#email').HTML('郵箱格式錯誤')
+          console.log('郵箱格式錯誤')
+        } else {
+          console.log('描述信息为空')
+          $('#advice').HTML('描述信息错误')
+        }
+      },
+      error: function () {
+        console.log('加盟失敗')
+      }
+    })
+  })
 })
