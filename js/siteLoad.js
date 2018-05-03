ee4c4079a0be7001d2059744d94ad3b16055f6ce
JunchunDai <daijunchun2675@126.com>
2018-05-03 14:05:56 +0800

    待解決 衝突問題

--- js/siteLoad.js
+++ js/siteLoad.js
@@ -1,60 +1,16 @@
 $(function () {
 
 
-  
 
   // 公共頁面加載
-   $('#top').load('../common/common.html')
-//    加盟合作界面提交
+  $('#top').load('../common/common.html')
 
-        // 表單提交
-        /* $("#btn").click(function () {
-            // var pwd = $("#ipt").val();
-            // var jm = $.md5(pwd)
-            // console.log(pwd + "+++++++++")
-            // console.log(jm + "+++++++++")
+  // 返回首页
+  $('#returnHomepage').click(function () {
+    window.location.replace('../../index.html')
+  })
 
-            function p(s) {
-                return s < 10 ? '0' + s : s;
-            }
+  // 表單提交
 
-            function GetNowTime() {
-                var myDate = new Date();
-                var year = myDate.getFullYear();
-                var month = myDate.getMonth() + 1;
-                var date = myDate.getDate();
-                var h = myDate.getHours();
-                var m = myDate.getMinutes();
-                var s = myDate.getSeconds();
-                return year + '-' + p(month) + "-" + p(date) + " " + p(h) + ':' + p(m) + ":" + p(s);
-            }
-            let requireData = $.md5("mixiusi" + GetNowTime());
-            $("#hide").attr("value", requireData);
-            var newData = $('#joinForm').serialize();
-            console.log(newData)
-            
-          $.ajax({
-                type: "POST",
-                url: 'http://mixiusi.com.cn:8081/feedback/addFeedback',
-                data: newData,
-                success: function (response) {
-                    console.log(newData)
-                    console.log("加盟成功了哈! 等待客服聯繫.")
-                    if(status=200){
-                        window.location.href='./submit_success/submitSucseccfully.html'
-                    }
-                },
-                error:function(request){
-                    console.log(newData)
-                    console.log("加盟失敗")
-                }
-            });
-        }); */
 
-// return honmepage
-
-$("#returnHomepage").click(function(){
-   
-    window.location.replace("../../index.html")
-})
 })
