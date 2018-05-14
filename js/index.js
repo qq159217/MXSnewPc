5fc8da022330c8bcae48a21bd63f600c39c94e57
JunchunDai <daijunchun2675@126.com>
2018-05-14 16:15:54 +0800

    form wancheng

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 $(function () {
-	$("#top").load("./site/common/common.html")
+  $('#top').load('./site/common/common.html')
 
   // 導航欄
   $('nav>ul .item').mousemove(function () {
@@ -7,20 +7,19 @@ $(function () {
     $(this).siblings().find('.sub-menu').hide()
   })
 
-//  全屏滚动
+  //  全屏滚动
 
   $('#dowebok').fullpage({
     // menu: '.topNav',
     css3: true,
-    menu:'#menu',
-    anchors:['firstPage', 'secondPage','thirdPage','fourthPage',"fifthPage","sixthPage"],
-    
-   
+    menu: '#menu',
+    anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'fifthPage', 'sixthPage']
+
+
 
   })
 
   // 表單提交
-  
   function p(s) {
     return s < 10 ? '0' + s : s;
 }
@@ -36,49 +35,43 @@ function GetNowTime() {
     return year + '-' + p(month) + '-' + p(date) + ' ' + p(h) + ':' + p(m)
 }
 
-var phoneNumber = $('#ph').val()
 
-var requireData = $.md5('mixiusi' + GetNowTime() + phoneNumber)
+
 
 $("#feedbackBtn").click(function () {
-
-    console.log(phoneNumber)
-    console.log(requireData + "表单数据")
-
-
+    var phoneNumber = $("#ph").val()
+    console.log(phoneNumber + "电话")
+    var requireData = $.md5('mixiusi' + GetNowTime() + phoneNumber)
+    console.log(requireData + "加密后的token")
     $('#hide').attr('value', requireData)
-    console.log(newData + "newData")
     var newData = $('#feedbackForm').serialize()
+    console.log(newData + "序列化数据")
+
+
     $.ajax({
         type: 'POST',
-        url: 'http://47.98.164.44:8081/feedback/addFeedback',
+        url: 'http://mixiusi.com.cn:8081/feedback/addFeedback',
         data: newData,
         success: function (result) {
-            if (result.code = '0000') {
-                console.log('反饋成功')
-                window.location.href =
-                    '../site/submit_success/submitSucseccfully.html'
-                // $('#reset').reset()
-            } else if (result.code = '0001') {
-                console.log('代碼執行異常')
-            } else if (result.code = '0002') {
-                console.log('电话号码格式錯誤')
-                $('#ph').HTML('电话号码格式错误')
-            } else if (result.code = '0003') {
-                console.log('用戶名為空')
-                $('#nickName').HTML('用戶名為空')
-            } else if (result.code = '0004') {
-                $('#email').HTML('郵箱格式錯誤')
-                console.log('郵箱格式錯誤')
-            } else {
-                console.log('描述信息为空')
-                $('#advice').HTML('描述信息错误')
+            if (result.code = '0001') {
+                console.log('反馈成功')
+                window.location.href = '../site/submit_success/submitSucseccfully.html'
+                $('#reset').reset()
             }
         },
-        error: function (responce) {
-            window.location.href =
-                '../site/submit_success/submitSucseccfully.html'
+
+        error: function (response) {
+            console.log("error" + "亲,重新尝试")
+
+
         }
+
+
     })
+
 });
+ 
+
+
+
 })
